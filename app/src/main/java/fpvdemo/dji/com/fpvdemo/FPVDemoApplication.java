d9cc411b671e721be3cdde6a42a1366bea73956c
miamiyayi <miamiyayi@gmail.com>
2017-06-06 14:51:51 -0400

    Initial commit

---
+++ app/src/main/java/fpvdemo/dji/com/fpvdemo/FPVDemoApplication.java
@@ -0,0 +1,8 @@
+package fpvdemo.dji.com.fpvdemo;
+import android.app.Application;
+public class FPVDemoApplication extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
